bcc71f7a00eb730bfebd48e79bb6033e2d732d89
cristiscorta <44902988+cristiscorta@users.noreply.github.com>
2018-11-20 10:56:22 -0800

    Add files via upload

---
+++ Warrior.java
@@ -0,0 +1,17 @@
+package heroes;
+
+public class Warrior extends Hero {
+    public Warrior(Integer energy, Integer life) {
+        super(energy, life);
+    }
+
+    @Override
+    public void takeDamage() {
+        life-=3;
+    }
+
+    @Override
+    public void attack() {
+        energy--;
+    }
+}
